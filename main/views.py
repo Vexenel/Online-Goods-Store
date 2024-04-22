2e5d3d2a150278e5cfbc277a89c1ceedfbc73e2a
Vexenel <vlad.m003@gmail.com>
2024-04-22 13:52:26 +0300

    First example

---
+++ main/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template.defaultfilters import first
+
+def index(request):
+    context: dict = {
+        'title': 'Home' ,
+        'content': 'Главная страница магазина - HOME',
+        'list' : ['first', 'second'],
+        'dict' : {'first': 1},
+        'is_authenticated': False
+    }
+
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    return HttpResponse('About page')
