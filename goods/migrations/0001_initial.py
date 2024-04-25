e59d4a7b4e4182e85c89f482165205a2f0e7304e
Vexenel <vlad.m003@gmail.com>
2024-04-25 17:34:24 +0300

    Fix

--- goods/migrations/0001_initial.py
+++ goods/migrations/0001_initial.py
@@ -31,7 +31,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=150, unique=True, verbose_name='Название')),
                 ('slug', models.SlugField(blank=True, max_length=200, null=True, unique=True, verbose_name='URL')),
-                ('discription', models.TextField(blank=True, null=True, verbose_name='Опиание')),
+                ('description', models.TextField(blank=True, null=True, verbose_name='Опиание')),
                 ('image', models.ImageField(blank=True, null=True, upload_to='goods_images', verbose_name='Изображение')),
                 ('price', models.DecimalField(decimal_places=2, default=0.0, max_digits=7, verbose_name='Цена')),
                 ('discount', models.DecimalField(decimal_places=2, default=0.0, max_digits=4, verbose_name='Скидка в процентах')),
--- goods/models.py
+++ goods/models.py
@@ -16,7 +16,7 @@ class Categories(models.Model):
 class Products(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
-    discription = models.TextField(blank=True, null= True, verbose_name='Опиание')
+    description = models.TextField(blank=True, null= True, verbose_name='Опиание')
     image = models.ImageField(upload_to='goods_images',blank=True, null= True, verbose_name='Изображение')
     price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
     discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name='Скидка в процентах')
