71a5ffc580ffe2ed3bf42f6845e10e51d5409451
Vexenel <vlad.m003@gmail.com>
2024-04-27 16:53:03 +0300

    Add login form

--- app/settings.py
+++ app/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     "debug_toolbar",
 
     'main',
-    'goods'
+    'goods',
+    'users',
     
 ]
 
@@ -68,7 +69,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -154,3 +155,5 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
